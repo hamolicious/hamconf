d049a4873c55a4b4694dfd4f82f992b69909b257
Viktors Patmalnieks <viktorspatmalnieks@comparethemarket.com>
2022-10-17 14:47:16 +0100

    slightly cleaner test running

--- unit_tests/__main__.py
+++ unit_tests/__main__.py
@@ -1,7 +1,10 @@
 import os
 
 path = __file__.replace(os.path.basename(__file__), '')
+all_files = ''
 for file in os.listdir('unit_tests/'):
 	filepath = os.path.join(path, file)
 	if not file.startswith('test_') : continue
-	os.system(f'python -m unittest -v {filepath}')
+	all_files += f' {filepath}'
+
+os.system(f'python -m unittest -v {all_files}')
